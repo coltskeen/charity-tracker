74f51c7146212ce43c50f543cd848b39ab62e3d8
Colt Skeen <colton.skeen@gmail.com>
2022-03-10 11:54:19 -0500

    Set up database and connected it to server
    CHANGES:
    - installed pg.pool and required it in server.js
    - created database charity-tracker
    - added migration.sql file and query.sql file
    - created table contributions and added initial data to test in migration.sql
    - queried data from query.sql

---
+++ query.sql
@@ -0,0 +1,3 @@
+-- SELECT * FROM contributions;
+
+SELECT SUM(amount) FROM contributions WHERE giver='Jason';
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
 const PORT = 5060;
+
+const pool = new Pool ({
+    database: "charity-tracker",
+});
 
 app.listen(PORT, () => console.log(`Listsening on PORT: ${PORT}`));
